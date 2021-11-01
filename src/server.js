d3dec924bd1cdbce7782c33bf131be8c2353e588
Rudy Eko Prasetya <rudyekoprasetya@yahoo.co.id>
2021-11-01 17:01:03 +0700

    [ADD] fitur users dan authentications

--- src/server.js
+++ src/server.js
@@ -9,8 +9,23 @@ const SongsService = require('./services/postgres/SongsService');
 //import validator Joi
 const SongsValidator = require('./validator/songs');
 
+//users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users')
+
+//authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications')
+
 const init = async() =>{
 	const songService = new SongsService();
+	//instance users
+	const usersService = new UsersService();
+	//instance auth
+	const authenticationsService = new AuthenticationsService();
 
 	const server = Hapi.server({
 		port:  process.env.PORT,
@@ -23,13 +38,31 @@ const init = async() =>{
 	});
 
 	//register plugin
-	await server.register({
-		plugin: songs,
-		options: {
-			service: songService,
-			validator: SongsValidator
-		}
-	});
+	await server.register([
+		{
+			plugin: songs,
+			options: {
+				service: songService,
+				validator: SongsValidator
+			},
+		},
+		{
+			plugin: users,
+			options: {
+			  service: usersService,
+			  validator: UsersValidator,
+			},
+		},
+		{
+			plugin: authentications,
+			options: {
+			  authenticationsService,
+			  usersService,
+			  tokenManager: TokenManager,
+			  validator: AuthenticationsValidator,
+			},
+		},
+	]);
 
 	await server.start();
 	console.log(`Server run at ${server.info.uri}`);
